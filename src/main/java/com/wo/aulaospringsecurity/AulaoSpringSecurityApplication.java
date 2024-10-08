a0dea79cc0a7345cac2ab554c9141f1b07c37f8a
wanderson648 <testepedroca@gmail.com>
2024-10-08 00:55:57 -0300

    implementa oauth-client

--- src/main/java/com/wo/aulaospringsecurity/AulaoSpringSecurityApplication.java
+++ src/main/java/com/wo/aulaospringsecurity/AulaoSpringSecurityApplication.java
@@ -2,6 +2,8 @@ package com.wo.aulaospringsecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,8 +24,29 @@ class HttpController {
     }
 
     @GetMapping("/private")
-    String privateRoute() {
-        return "<h1>Public route, only authorized personal! 🔒 </h1>";
+    String privateRoute(@AuthenticationPrincipal OidcUser principal) {
+        return """
+                <h1>Private route, only authorized personal!</h1>
+                """;
+    }
+
+    @GetMapping("/cookie")
+    String cookie(@AuthenticationPrincipal OidcUser principal) {
+        return String.format("""
+                <h1>Oauth2 </h1>
+                <h3>Principal: %s</h3>
+                <h3>Email attribute: %s</h3>
+                <h3>Authorities: %s</h3>
+                <h3>JWT: %s</h3>
+                """, principal, principal.getAttribute("email"), principal.getAuthorities(),
+        principal.getIdToken().getTokenValue());
+    }
+
+    @GetMapping("/jwt")
+    String jwt(@AuthenticationPrincipal OidcUser principal) {
+        return """
+                <h1>JWT </h1>
+                """;
     }
 
 }
--- src/main/java/com/wo/aulaospringsecurity/SecurityConfig.java
+++ src/main/java/com/wo/aulaospringsecurity/SecurityConfig.java
@@ -20,7 +20,7 @@ public class SecurityConfig {
                             authorizeConfig.requestMatchers("/logout").permitAll();
                             authorizeConfig.anyRequest().authenticated();
                         })
-                .formLogin(Customizer.withDefaults())
+                .oauth2Login(Customizer.withDefaults())
                 .build();
     }
 
